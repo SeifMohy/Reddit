1b5334924d7e3dc40494e49c69ad226f37b6cc9b
SeifMohy <seifmohy@gmail.com>
2022-03-24 20:21:41 +0200

    reddit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navigation from "./Components/Navigation";
+import Posts from "./Components/Posts";
+import PostDetails from "./Components/PostDetails"
+import Box from "@mui/material/Box";
+import { grey } from "@mui/material/colors";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getPosts } from "./Actions/PostActions";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getPosts());
+  }, []);
+  const state = useSelector(state => state.posts)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ bgcolor: grey[200]}}>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Posts />}/>
+        <Route path="/:id" element={<PostDetails />}/>
+      </Routes>
+    </Box>
   );
 }
 
